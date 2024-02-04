b80c3b773787569e635bdf19349332042a5b1c3b
kashyap0078 <dineshkashyap961@gmail.com>
2024-02-04 14:30:17 +0530

    changes api for add product
    changes api for add product

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../servises/products.service';
+import { product } from 'userSignup';
 
 @Component({
   selector: 'app-seller-add-product',
@@ -6,13 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seller-add-product.component.css']
 })
 export class SellerAddProductComponent implements OnInit {
-
-  constructor() { }
+  addProductMessage: string | undefined
+  constructor(private product: ProductsService) { }
 
   ngOnInit(): void {
-
   }
-  submit(data:any){
-    console.log(data);
+  submit(data:product){
+    this.product.addProduct(data).subscribe((result)=>{
+      console.log(result);
+      if(result){
+        this.addProductMessage = "The Product has been added sucessfully"
+      }
+      
+    })
+    setTimeout(() =>{
+     this.addProductMessage = undefined
+    },5000)
+
+      
   }
 }
--- userSignup.ts
+++ userSignup.ts
@@ -1,9 +1,19 @@
-export interface signUp{
+export interface signUp {
     name: string,
     email: string,
-    password:string
+    password: string
 }
-export interface login{
+export interface login {
     email: string,
-    password:string
+    password: string
+}
+
+export interface product {
+    Productname: string,
+    price: number,
+    category: string,
+    color: string,
+    description: string,
+    image: string
+
 }
